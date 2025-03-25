eb980a5f6ea324ef4b6df53dfaab79c52b9a29b5
Samaila <41795852+DevRelMalima@users.noreply.github.com>
2025-03-25 16:40:24 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,10 +5,10 @@ const config = {
   solidity: "0.8.20",
   networks: {
     assetHub: {
-      url: "https://moonbeam-alpha.api.onfinality.io/rpc?apikey=3c5eeeda-61a9-415c-aea2-4dff37496677", // Replace with the correct RPC endpoint
+      url: "https://rpc.polkadot-asset-hub.testnet", // Replace with the correct RPC endpoint
       chainId: 1287, // Moonbase Alpha Testnet chain ID
       accounts: {
-        mnemonic: "craft bullet clay staff chef twist two chuckle pact pretty more salute", // Replace with your mnemonic
+        mnemonic: "your-mnemonic-here", // Replace with your mnemonic
       },
     },
   },
